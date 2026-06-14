6175399f62c00837c71e5d2ca87c1ce19e6e3c8c
guilhermeguedes01 <guilherme.guedes@cazia.ai>
2026-06-14 14:14:40 -0300

    feature(cors): enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Orbit API')
     .setDescription('Api for agency, artists, invites, and events management')
