dd9510cef63a96a984faffc052cf52243106893e
guilhermeguedes01 <guilherme.guedes@cazia.ai>
2026-06-24 03:02:09 -0300

    feat(artist):  add find artist by id

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guards';
 import { CurrentUserDto } from '../auth/dtos/user.dto';
 import { CurrentUser } from 'src/auth/decorators/user.decorator';
@@ -73,5 +73,21 @@ export class ArtistsController {
   @Get('me')
   me(@CurrentUser() user: CurrentUserDto) {
     return this.artistsService.getMe(user);
+  }
+
+  @ApiOperation({
+    summary: 'Show artist by id',
+    description: 'Returns artist profile linked to the authenticated user.',
+  })
+  @ApiOkResponse({
+    description: 'Artist data returned successfully.',
+  })
+  @ApiUnauthorizedResponse({ description: 'Missing or invalid bearer token.' })
+  @ApiNotFoundResponse({
+    description: 'The authenticated user does not have an artist profile.',
+  })
+  @Get(':id')
+  getArtistById(@Param('id') id: string, @CurrentUser() user: CurrentUserDto) {
+    return this.artistsService.getArtistById(id, user);
   }
 }
